2cff4cbd7e9733fb3c8d4305f47bfe415cf71476
桥下一粒砂 <chenyoca@gmail.com>
2014-06-27 14:09:03 +0800

    To Android Studio Project

---
+++ app/src/main/java/gmail/chenyoca/imagemap/app/HighlightImageActivity.java
@@ -0,0 +1,34 @@
+package gmail.chenyoca.imagemap.app;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.Bundle;
+
+import gmail.chenyoca.imagemap.HighlightImageView;
+import gmail.chenyoca.imagemap.support.CircleShape;
+
+/**
+ * author : 桥下一粒砂 (chenyoca@gmail.com)
+ * date   : 2013-06-07
+ * 图片高亮区域演示Activity
+ */
+public class HighlightImageActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_highlight_image);
+		HighlightImageView imageView = (HighlightImageView) findViewById(R.id.image);
+		Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.imm_01, new BitmapFactory.Options());
+		imageView.setImageBitmap(bitmap);
+
+		CircleShape shape = new CircleShape("Tag", Color.RED);
+		shape.setValues(100,100,20);
+
+		imageView.addShape(shape);
+
+	}
+}
---
+++ app/src/main/java/gmail/chenyoca/imagemap/app/TouchImageActivity.java
@@ -0,0 +1,26 @@
+package gmail.chenyoca.imagemap.app;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+
+import gmail.chenyoca.imagemap.TouchImageView;
+
+/**
+ * author : 桥下一粒砂 (chenyoca@gmail.com)
+ * date   : 2013-06-07
+ * 演示TouchImageView的Activity
+ */
+public class TouchImageActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_imageview);
+		TouchImageView imageView = (TouchImageView) findViewById(R.id.image);
+		Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.imm_01, new BitmapFactory.Options());
+		imageView.setImageBitmap(bitmap);
+
+	}
+}
